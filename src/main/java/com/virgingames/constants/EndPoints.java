5f8420b6f39f54fe3f88e27d13df84b655dc3d6a
priyankagajera <prgajera2427@gmail.com>
2024-02-02 11:19:31 +0000

    My homework

---
+++ src/main/java/com/virgingames/constants/EndPoints.java
@@ -0,0 +1,10 @@
+package com.virgingames.constants;
+
+public class EndPoints {
+
+    /**
+     * This is Endpoints of virgin games
+     */
+    public static final String CURRENCYGBP = "?currency=GBP";
+    public static final String CURRENCYEUR = "?currency=EUR";
+}
